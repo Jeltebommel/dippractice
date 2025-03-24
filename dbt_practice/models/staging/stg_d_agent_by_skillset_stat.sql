cddf0223846609703761bd64a4cadaf15dd63f8c
Jeltebommel <jeltebommel@live.nl>
2025-03-24 09:37:10 +0100

    first stage

---
+++ dbt_practice/models/staging/stg_d_agent_by_skillset_stat.sql
@@ -0,0 +1,61 @@
+{{ config(
+    materialized='table',
+    schema='jelte'
+) }}
+
+WITH raw AS (
+    SELECT
+        *
+    FROM {{ source('jelte_raw', 'd_agent_by_skillset_stat') }}
+)
+
+SELECT
+    -- renaming the columns in proper format
+    raw.Intern_ID                       AS intern_id,
+    raw.AgentGivenName                  AS agent_given_name,
+    raw.AgentLogin                      AS agent_login,
+    raw.AgentSurName                    AS agent_surname,
+    raw.BlendedActiveTime               AS blended_active_time,
+    raw.Call_Only                       AS call_only,        
+    raw.Call_Type                       AS call_type,
+    raw.CallsAnswered                   AS calls_answered,
+    raw.CallsConferenced                AS calls_conferenced,
+    raw.CallsOffered                    AS calls_offered,
+    raw.CallsReturnToQ                  AS calls_return_to_q,
+    raw.CallsReturnToQDueToTimeout      AS calls_return_to_q_due_to_timeout,
+    raw.CallsTransferred                AS calls_transferred,
+    raw.ConsultTime                     AS consult_time,
+    raw.ContactHoldTime                 AS contact_hold_time,
+    raw.ContactTalkTime                 AS contact_talk_time,
+    raw.ContactType                     AS contact_type,
+    raw.DNOutExtTalkTime               AS dn_out_ext_talk_time,
+    raw.DNOutIntTalkTime               AS dn_out_int_talk_time,
+    raw.HoldTime                        AS hold_time,
+
+    -- Columns from page 2
+    raw.IdleTime                        AS idle_time,
+    raw.Lastname                        AS last_name,
+    raw.MaxCapacityIdleTime             AS max_capacity_idle_time,
+    raw.MaxCapacityTotalStaffedTime     AS max_capacity_total_staffed_time,
+    raw.NotReadyTime                    AS not_ready_time,
+    raw.PostCallProcessingTime          AS post_call_processing_time,
+    raw.RingTime                        AS ring_time,
+    raw.ShortCallsAnswered              AS short_calls_answered,
+    raw.Site                            AS site,
+    raw.SiteID                          AS site_id,
+    raw.Skillset                        AS skillset,
+    raw.SkillsetID                      AS skillset_id,
+    raw.SubIdleTime_Voice               AS sub_idle_time_voice,
+    raw.SubIdleTime_WC                 AS sub_idle_time_wc,
+    raw.TalkTime                        AS talk_time,
+    raw.Time                            AS time_event,          
+    raw.Timestamp                       AS event_timestamp,
+    raw.TotalStaffedTime                AS total_staffed_time,
+    raw.UserID                          AS user_id,
+    raw.Werkdag                         AS werkdag,
+
+    
+    raw.WaitTime                        AS wait_time,
+    raw.Werkweek                        AS werkweek
+
+FROM raw
---
+++ dbt_practice/models/staging/stg_medewerkerslijst.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='table',
+    schema='jelte'
+) }}
+
+WITH raw AS (
+    SELECT
+        *
+    FROM {{ source('jelte_raw', 'medewerkerslijst') }}
+)
+
+SELECT
+    raw.`Intern_ID`       AS intern_id,
+    raw.`Achternaam`      AS last_name,
+    raw.`Achtervoegsel`   AS suffix,
+    raw.`Actief`          AS actief,
+    raw.`Year`            AS year,
+    raw.`Quarter`         AS quarter,
+    raw.`Month`           AS month,
+    raw.`Day`             AS day_of_month,
+    raw.`BPMS_User`       AS bpms_user,
+    raw.`Dagen_in_dienst` AS days_in_service,
+    raw.`Dienstweek`      AS dienstweek,
+    raw.`E-mail`          AS email,         -- MUST be quoted because of the dash
+    raw.`Groep`           AS groep,
+    raw.`Filter`          AS filter_val,
+    raw.`Is_supervisor`   AS is_supervisor
+FROM raw
+
